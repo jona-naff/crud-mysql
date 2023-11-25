895e0aaec695d6fe24f93f454b52527f681233f4
Jonanaff <jonanaff97outlook.com>
2023-11-25 17:57:18 -0600

    first commit

---
+++ djstudent/students/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+from students.forms import StudentsForm
+from students.models import Students
+
+# Create your views here.
+
+def std(request):
+    if request.method == "POST":
+        form = StudentsForm(request.POST)
+        if form.is_valid():
+            try:
+                form.save()
+                return redirect('/views')
+            except:
+                pass
+    else:
+        form = StudentsForm()
+    return render(request,'index.html', {'form': form})
